18db202e2c815dff42f90febe71e4d059903268a
Abdul-Azeez <zeezu124@gmail.com>
2023-10-05 07:48:21 +0100

    Added TodoList Controller

---
+++ app/Http/Controllers/TodoListController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Models\ListItem;
+
+
+class TodoListController extends Controller
+{
+    public function index () {
+        return view('welcome', ['listItems' => ListItem::all()]);
+    }
+
+    public function saveItem (Request $request) {
+
+        Log::info(json_encode($request->all()));
+
+        $newListItem = new ListItem();
+        $newListItem->name = $request->listItem;
+        $newListItem->is_complete = 0;
+        $newListItem->save();
+
+        return view('welcome');
+    }
+}
