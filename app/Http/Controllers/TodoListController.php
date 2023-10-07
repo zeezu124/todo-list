c31497de2718a1bc4d9f03c38b67c7b65e5e265d
Abdul-Azeez <zeezu124@gmail.com>
2023-10-07 14:51:14 +0100

    added mark complete functionality

--- app/Http/Controllers/TodoListController.php
+++ app/Http/Controllers/TodoListController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\ListItem;
-
+use Illuminate\Support\Facades\Log;
 
 class TodoListController extends Controller
 {
@@ -14,10 +14,22 @@ class TodoListController extends Controller
 
     public function saveItem (Request $request) {
 
+        #Creating new item
         $newListItem = new ListItem;
         $newListItem->name = $request->listItem;
         $newListItem->is_complete = 0;
+
         $newListItem->save();
+
+        return redirect('/');
+    }
+
+    public function markComplete ($id) {
+
+        #changing is_complete flag to 1
+        $listItem = ListItem::find($id);
+        $listItem->is_complete = 1;
+        $listItem->save();
 
         return redirect('/');
     }
